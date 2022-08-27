116b7e14ae5db2842027237cddc411d265fdc3e5
Your Name <peyton@spreacker.com>
2022-08-27 09:14:58 -0500

    initial commit

---
+++ practicePy_22.py
@@ -0,0 +1,9 @@
+# open file to read
+fhand = open("./SUN.txt")
+
+# print line count to check integrity of the file handle
+# count = 0
+# for line in fhand:
+#     count = count +1
+# print("Line Count:", count)
+
